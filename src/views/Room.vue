d68144a2d187659e47889b43ff37c211f229abc7
Yilee <yileefrontend@gmail.com>
2024-05-10 21:59:37 +0800

    [介紹]加入日期選擇組件及運算

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -6,23 +6,75 @@
   <section class="room-detail-container">
     <Introduction class="introduction" :room-details="roomStore.roomDetail" />
     <FacilityInformation :facility-info="roomStore.facilityInfo" />
+    <section class="order-area">
+      <div class="day-price-area">
+        <DateRangePicker
+          :disabled-days="roomStore.bookedDays"
+          @update-between-dates="setDaysPrice"
+        />
+        <div class="prices">
+          <p title="Room">
+            <span>{{ prices.default }}</span>
+          </p>
+          <p title="Tax">
+            <span :style="{ width: `${prices.width}px` }">{{ prices.tax }}</span>
+          </p>
+          <p title="Total">
+            <span id="PriceTotalWidth">{{ prices.total }}</span>
+          </p>
+        </div>
+      </div>
+    </section>
   </section>
 </template>
 
 <script setup lang="ts">
+import { ref, nextTick } from 'vue';
+import router from '@/router';
 import { useRoute } from 'vue-router';
 import { useRoomStore } from '@/stores/bookingRoom';
 import Carousel from '@/components/Room/Carousel.vue';
 import Introduction from '@/components/Room/Introduction.vue';
 import FacilityInformation from '@/components/Room/FacilityInformation.vue';
-import router from '@/router';
+import DateRangePicker from '@/components/DateRangePicker.vue';
+import { priceAddCommas } from '@/utils/priceAddCommas';
+import { calculateDaysPrice } from '@/utils/daysControlMehods';
 
 const route = useRoute();
 const routeParamsId = route.params.id.toString();
 
+//價格計算
+const prices = ref<{
+  default: string;
+  tax: string;
+  total: string;
+  width: number;
+}>({
+  default: '0',
+  tax: '0',
+  total: '0',
+  width: 50
+});
+
 //獲取房間資料
 const roomStore = useRoomStore();
 roomStore.fetchRoom(routeParamsId);
+
+//計算住宿金額
+const setDaysPrice = (betweenDates: string[]) => {
+  const roomPrice = calculateDaysPrice(
+    betweenDates,
+    roomStore.roomDetail.weekdayPrice,
+    roomStore.roomDetail.weekendPrice
+  );
+  prices.value.default = priceAddCommas(roomPrice);
+  prices.value.tax = priceAddCommas(Math.ceil(roomPrice * 0.05));
+  prices.value.total = priceAddCommas(roomPrice + Math.ceil(roomPrice * 0.05));
+  nextTick(() => {
+    const priceElement = document.getElementById('PriceTotalWidth') as HTMLElement | null;
+    prices.value.width = priceElement ? priceElement.getBoundingClientRect().width : 0;
+  });
+};
 </script>
 
 <style scoped lang="scss">
@@ -45,6 +97,40 @@ header {
 
   .introduction {
     margin: 2rem 0;
+  }
+}
+.order-area {
+  display: flex;
+  margin: 60px 0 30px;
+  .day-price-area {
+    width: 294px;
+    .prices {
+      p {
+        display: flex;
+        justify-content: space-between;
+        margin: 0;
+
+        &:before {
+          content: attr(title);
+        }
+        &:nth-child(2) {
+          margin-top: 10px;
+          margin-bottom: 20px;
+        }
+        &:last-child {
+          color: #273036;
+        }
+        span {
+          display: flex;
+          justify-content: space-between;
+          gap: 5px;
+          min-width: 50px;
+          &:before {
+            content: 'NT$';
+          }
+        }
+      }
+    }
   }
 }
 </style>
